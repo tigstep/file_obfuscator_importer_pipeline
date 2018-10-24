20991561929c887cf841e0793c74ba017ceb62a0
tigstep <tigstep@gmail.com>
2018-10-23 20:20:00 -0700

    adding second subnet adding internet gateway adding separate AZs adding an RDS

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,9 @@ variable "region" {}
 variable "shared_credentials_file" {}
 variable "profile" {}
 variable "sfn_triggerer_lambda_function" {}
+variable "rds_pass" {}
+
+data "aws_availability_zones" "all" {}
 
 ################################################################
 
@@ -26,7 +29,9 @@ provider "aws" {
 ################################################################
 
 resource "aws_vpc" "terraform" {
+
   cidr_block       = "10.0.0.0/16"
+  enable_dns_hostnames = true
   tags {
     name = "frbhackathon2018"
   }
@@ -64,19 +69,65 @@ resource "aws_security_group" "allow_all" {
 ################################################################
 
 ################################################################
-# Defining a subnet                                            #
+# Defining a subnets                                           #
 ################################################################
 
 resource "aws_subnet" "main" {
     vpc_id     = "${aws_vpc.terraform.id}"
     cidr_block = "10.0.1.0/24"
+    availability_zone = "${data.aws_availability_zones.all.names[0]}"
 
     tags {
       name = "frbhackathon2018"
     }
 }
 
+resource "aws_subnet" "secondary" {
+    vpc_id     = "${aws_vpc.terraform.id}"
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "${data.aws_availability_zones.all.names[1]}"
+    tags {
+        name = "frbhackathon2018"
+    }
+}
+
 ################################################################
+
+################################################################
+# Defining an internet gateway                                 #
+################################################################
+
+resource "aws_internet_gateway" "gw" {
+     vpc_id = "${aws_vpc.terraform.id}"
+
+     tags {
+        name = "frbhackathon2018"
+     }
+}
+
+################################################################
+
+resource "aws_route_table" "public-rt" {
+    vpc_id = "${aws_vpc.terraform.id}"
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags {
+    name = "frbhackathon2018"
+  }
+}
+
+resource "aws_route_table_association" "public-rt-1" {
+    subnet_id = "${aws_subnet.main.id}"
+    route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+resource "aws_route_table_association" "public-rt-2" {
+    subnet_id = "${aws_subnet.secondary.id}"
+    route_table_id = "${aws_route_table.public-rt.id}"
+}
 
 ################################################################
 # Defining the lambda role                                     #
@@ -223,6 +274,42 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "source/"
   }
+}
+
+################################################################
+
+################################################################
+# creating subnet_group                                        #
+################################################################
+
+resource "aws_db_subnet_group" "subnet_group" {
+    name        = "subnet_group"
+    description = "subnet group for RDS"
+    subnet_ids  = ["${aws_subnet.main.id}","${aws_subnet.secondary.id}"]
+}
+
+################################################################
+# adding a RDS instance                                        #
+################################################################
+
+resource "aws_db_instance" "frbhackathon2018tf" {
+    allocated_storage    = 10
+    identifier           = "frbhackathon2018tf"
+    storage_type         = "gp2"
+    engine               = "mysql"
+    engine_version       = "5.7.23"
+    instance_class       = "db.t2.micro"
+    name                 = "frbhackathon2018tf"
+    username             = "root"
+    password             = "${var.rds_pass}"
+    parameter_group_name = "default.mysql5.7"
+    publicly_accessible  = true
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    db_subnet_group_name   = "${aws_db_subnet_group.subnet_group.id}"
+    skip_final_snapshot = true
+    tags {
+        name = "frbhackathon2018"
+    }
 }
 
 ################################################################
