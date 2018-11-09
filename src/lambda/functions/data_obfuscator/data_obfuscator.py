8af5e2868ea6398604c16b8c7999f997ffd50bc3
tigstep <tigstep@gmail.com>
2018-11-08 20:55:06 -0800

    file_obfuscator cnt

--- src/lambda/functions/data_obfuscator/data_obfuscator.py
+++ src/lambda/functions/data_obfuscator/data_obfuscator.py
@@ -11,14 +11,16 @@ def lambda_handler(event, context):
         WHERE PII_FLG = 'Y'
         ;
     '''
+    BUCKET_NAME = 'frbhackathon2018tf' # replace with your bucket name
+    KEY = 'source/test.txt' # replace with your object key
     def create_connection():
         connection = mysql.connector.connect(user='user',
                                              password='password',
                                              host='host',
                                              database='database')
         return connection
+
     def get_pii_fields():
-        #conn_params = load_json()
         connection = create_connection()
         cursor = connection.cursor()
         cursor.execute(QUERY)
@@ -28,4 +30,11 @@ def lambda_handler(event, context):
         for x in myresult:
             PII_ARR.append(str(x[0]))
         return pii_field_arr
-    def obfuscate():
+
+    def copy_from_s3():
+        s3 = boto3.resource('s3')
+        s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/test.txt')
+        return
+
+    copy_from_s3()
+
